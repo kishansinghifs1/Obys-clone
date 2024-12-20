357b96065de49ae26e0fc86f5c31d89918d53c86
kishansinghifs1 <kishansingh956196@gmail.com>
2024-12-20 12:34:18 +0530

    error

--- script.js
+++ script.js
@@ -30,20 +30,25 @@ function loadingloader(){
     })
     t1.from("#page1 , #page2" ,{
          delay : 0.1,
-         y:1600,
-         duration : 0.4,
-         ease:Power4,
+         y:1200,
+         opacity:0,
+         duration : 0.6,
+         ease:Power0,
     })
     t1.to("#loader",{
         display: "none",
     })
     t1.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
-        y:120,
-        stagger:0.2
+      y:1200,
+      stagger:0.4
     })
     t1.from("#nav",{
         opacity : 0
     })
+    t1.from("#hero1,#page2",{
+      opacity : 0,
+    }, "-= 1") 
+
 }
 loadingloader();
 function cursoranimation(){
@@ -57,3 +62,88 @@ function cursoranimation(){
       });
 }
 cursoranimation();
+function locomotive(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// --- RED PANEL ---
+gsap.from(".line-1", {
+  scrollTrigger: {
+    trigger: ".line-1",
+    scroller: "#main",
+    scrub: true,
+    start: "top bottom",
+    end: "top top",
+    onUpdate: self => console.log(self.direction)
+  },
+  scaleX: 0,
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+// --- ORANGE PANEL ---
+gsap.from(".line-2", {
+  scrollTrigger: {
+    trigger: ".orange",
+    scroller: "#main",
+    scrub: true,
+    pin: true,
+    start: "top top",
+    end: "+=100%"
+  },
+  scaleX: 0, 
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+// --- PURPLE/GREEN PANEL ---
+var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".purple",
+      scroller: "#main",
+      scrub: true,
+      pin: true,
+      start: "top top",
+      end: "+=100%"
+    }
+});
+
+tl.from(".purple p", {scale: 0.3, rotation: 45, autoAlpha: 0, ease: "power2"})
+  .from(".line-3", {scaleX: 0, transformOrigin: "left center", ease: "none"}, 0)
+  .to(".purple", {backgroundColor: "#28a92b"}, 0);
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+}
+// locomotive()
+function Photo1(){
+   Shery.imageEffect(".image-div" ,{
+    style : 5,
+    config : {"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272614206201047},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    gooey : true,
+   })
+}
+Photo1();
